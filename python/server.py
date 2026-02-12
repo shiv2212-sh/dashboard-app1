487f7728c4cd92429f0782d183fd0c11a20b901f
shiv2212-sh <shivamchawdhry8@gmail.com>
2026-02-12 16:14:28 +0530

    Update server.py

--- python/server.py
+++ python/server.py
@@ -1175,8 +1175,7 @@ import json, datetime, io
 
 app = Flask(__name__)
 
-DATABASE_URL = "postgresql://dashboard_db_01ci_user:fhQvDMPEzMEZJxyw7mJ3Vb951ChKBDj7@dpg-d64a6enfte5s7382kon0-a/dashboard_db_01ci"
-
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db():
     return psycopg2.connect(DATABASE_URL, sslmode="require")
